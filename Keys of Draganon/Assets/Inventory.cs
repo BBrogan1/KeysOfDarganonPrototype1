ec0b3e628bbdde285eaa7e3ce1013adeb5d2c010
BBrogan1 <g00357311@gmit.ie>
2020-12-01 13:35:11 +0000

    commit
    protoytpe

---
+++ Keys of Draganon/Assets/Inventory.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Inventory : MonoBehaviour
+{
+
+    public static Inventory instance;
+
+    void Awake()
+    {
+        if (instance != null)
+        {
+            Debug.LogWarning("More than one instance of inventory found!");
+            return;
+        }
+        instance = this;
+    }
+
+    public delegate void OnItemChanged();
+    public OnItemChanged OnItemChangedCallBack;
+
+    public int space = 20;
+
+    public List<Item> items = new List<Item>();
+
+    public bool Add (Item item)
+    {
+        if (!item.isDefaultItem)
+            {
+            if (items.Count >= space)
+            {
+                Debug.Log("Not enough space.");
+                return false;
+            }
+                items.Add(item);
+
+            if (OnItemChangedCallBack != null)
+                OnItemChangedCallBack.Invoke();
+            }
+        return true;
+    }
+
+    public void Remove (Item item)
+    {
+        items.Remove(item);
+
+        if (OnItemChangedCallBack != null)
+            OnItemChangedCallBack.Invoke();
+    }
+}
